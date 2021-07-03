6056c2a07383dd34fe24230af5ed4c8e1d3a9239
nvxden <clever411@yandex.ru>
2021-07-03 16:39:05 +0300

    Added distr/poisson.py

---
+++ distr/poisson.py
@@ -0,0 +1,41 @@
+############################################################
+# imports 
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+
+from funs import *
+
+
+
+
+
+############################################################
+# data
+def poisson(k, l):
+	return l ** k / fac(k) * math.exp(-l)
+
+n = 40
+
+charts = [ 5, 10, 20 ]
+
+for i in range(len(charts)):
+	x = list(range(0, n+1))
+	y = [ poisson(k, charts[i]) for k in x ]
+
+	charts[i] = (x, y, 'lambda = %i' % charts[i])
+
+
+
+
+
+############################################################
+# plot
+plot(charts)
+
+
+
+
+
+############################################################
+# END
