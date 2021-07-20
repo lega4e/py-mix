dad1399dcbfe607db054fae26baf614ac5055a10
nvxden <clever411@yandex.ru>
2021-07-21 00:14:41 +0300

    obstagctl: v0.2: Added argument parsing

--- obstagctl/main.py
+++ obstagctl/main.py
@@ -79,8 +79,93 @@ def replace_tags(data, repmap):
 
 
 ############################################################
+# init argparse
+def create_parser():
+	parser = argparse.ArgumentParser(description='Программа для управления тегами в MarkDown-заметках')
+
+	parser.add_argument(
+		'-f',
+		'--file',
+		dest = 'file',
+		type = str,
+		help = 'Указать файл для обработки'
+	)
+
+	parser.add_argument(
+		'-d',
+		'--directory',
+		dest = 'dir',
+		type = str,
+		help = 'Указать директорию, все файлы в которой будут обработаны ' +
+			   '(укажите -r, чтобы обработать директорию рекурсивно)'
+	)
+
+	parser.add_argument(
+		'-r',
+		'--recursive',
+		dest   = 'recursive',
+		action = 'store_true',
+		help   = 'Обработать указанную папку рекурсивно'
+	)
+
+	parser.add_argument(
+		'-c',
+		'--count',
+		dest   = 'count',
+		action = 'store_true',
+		help   = 'Посчитать количество тегов и выдать в формате json'
+	)
+
+	parser.add_argument(
+		'-m',
+		'--map',
+		dest = 'map',
+		type = str,
+		help = 'Заменить теги на другие; аргумент должен представлять из себя ' +
+		'словарь в формате json. Пример: \'{ "определения" : "определения", "раз" : "два" }\''
+	)
+
+	parser.add_argument(
+		'-M',
+		'--no-md-only',
+		dest   = 'nomdonly',
+		action = 'store_true',
+		help   = 'Обрабатывать все файлы, а не только с расширением md'
+	)
+
+	return parser
+
+
+
+
+
+############################################################
 # main
-replace_tags(open(dname + '/Смерть-советчик.md').read(), {'#эзотерика' : '#чушь'})
+parser = create_parser()
+args = parser.parse_args()
+
+files = []
+
+if args.file is not None:
+	files.append(args.file)
+
+if args.dir is not None:
+	if args.recursive:
+		recursive(
+			args.dir,
+			lambda f: files.append(f),
+			onlyfiles=True
+		)
+
+	else:
+		for fname in os.listdir(args.dir):
+			files.append(fname)
+
+if not args.nomdonly:
+	files = list(filter(lambda f: len(f) > 2 and f[-2:] == 'md', files))
+
+print(files)
+
 
 
 
