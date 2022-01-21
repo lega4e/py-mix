bbbc5d0324a9f2f662a197d3715aafba15230e8b
nvxden <clever411@yandex.ru>
2022-01-21 05:01:05 +0300

    mp3tags: write accessory functions

--- mp3tags/mp3tags.py
+++ mp3tags/mp3tags.py
@@ -15,17 +15,35 @@
 # теги с помощью утилиты id3v2
 #
 
+import argparse as ap
 import os
 import re
+import subprocess
 import sys
-import argparse as ap
 
 
 
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-# ~~~~~                        argument parser                         ~~~~~ #
+# ~~~~~                           CONSTANTS                            ~~~~~ #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+TIT2 = 'TIT2' # Название
+TRCK = 'TRCK' # Номер трека и общее количество
+TPE1 = 'TPE1' # Исполнитель
+TPE2 = 'TPE2' # Исполнитель альбома
+TALB = 'TALB' # Альбом
+TCON = 'TCON' # Жанр
+PCNT = 'PCNT' # Количество прослушиваний
+TYER = 'TYER' # Год
+
+
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# ~~~~~                        ARGUMENT PARSER                         ~~~~~ #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
 def make_parser():
@@ -50,7 +68,7 @@ def make_parser():
 
 	parser.add_argument(
 		'-T', '--total-tracks-auto', dest='totalauto', default=False,
-		action='store_false',
+		action='store_true',
 		help='Установить количество треков в альбоме по количеству ' +
 		     'обрабатываемых файлов'
 	)
@@ -88,7 +106,7 @@ def make_parser():
 
 	parser.add_argument(
 		'-s', '--sort-genres', dest='sortgenres', default=False,
-		action='store_false',
+		action='store_true',
 		help='Отсортировать жанры'
 	)
 
@@ -105,7 +123,7 @@ def make_parser():
 
 	parser.add_argument(
 		'-p', '--parse-filename', dest='parsefilename', default=False,
-		action='store_false',
+		action='store_true',
 		help='Извлекает из названия файла номер трека (опционально) ' +
 		     'исполнителя и название (подробнее см. регулярку в исходниках); ' +
 				 'если установлены другие флаги, записывающие те же поля, то ' +
@@ -113,8 +131,15 @@ def make_parser():
 	)
 
 	parser.add_argument(
+		'-L', '--same-artist', dest='sameartist', default=True,
+		action='store_true',
+		help='При получении полей из имени файла устанавливать не только ' +
+		     'исполнителя, но и исполнителя альбома'
+	)
+
+	parser.add_argument(
 		'-v', '--verbose', dest='verbose', default=False,
-		action='store_false',
+		action='store_true',
 		help='Выводить каждую исполняемую команду'
 	)
 
@@ -125,30 +150,29 @@ def make_parser():
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-# ~~~~~                             config                             ~~~~~ #
+# ~~~~~                             CONFIG                             ~~~~~ #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
 def make_config(args: ap.Namespace):
 	cfg        = args
 	cfg.nameex = re.compile(r'^(?:(\d+)\.)?\s*(.*)\s* - \s*(.*)\.mp3$')
-	cfg.genrex = re.compile(r'[\w\s]+')
 	cfg.setcom = 'id3v2 -2 --%s "%s" "%s"'
 	cfg.getcom = 'id3v2 -R "%s" | grep "%s" | cut -d' ' -f2- | sed -e "s/\s\+([0-9]\+)$//"'
 	cfg.files  = ( args.files if len(args.files) != 0 else
 		[ f for f in os.listdir() if re.match(r'.*\.mp3', f) ] )
 	cfg.fields = list(filter(lambda x: x[1] is not None, [
-		( 'TIT2', args.name        ),
-		( 'TRCK', '%s%s' % (
+		( TIT2, args.name        ),
+		( TRCK, '%s%s' % (
 			str(args.num or ''),
 			'/' + str(args.total)
 				if args.total else ''
 		) or None                  ),
-		( 'TPE1', args.artist      ),
-		( 'TPE2', args.albumartist ),
-		( 'TALB', args.album       ),
-		( 'TCON', args.genre       ),
-		( 'PCNT', args.playcount   ),
-		( 'TYER', args.year        ),
+		( TPE1, args.artist      ),
+		( TPE2, args.albumartist ),
+		( TALB, args.album       ),
+		( TCON, args.genre       ),
+		( PCNT, args.playcount   ),
+		( TYER, args.year        ),
 	]))
 
 	return args
@@ -158,7 +182,61 @@ def make_config(args: ap.Namespace):
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-# ~~~~~                              main                              ~~~~~ #
+# ~~~~~                      ACCESSORY FUNCTIONS                       ~~~~~ #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+def set_field(cfg, frame, value, file):
+	com = cfg.com % (frame, value, file)
+	cfg.verbose and print(com)
+	return -1 if os.system(com) != 0 else 0
+
+
+
+def set_fields_by_filename(cfg, file):
+	m = re.match(cfg.tmpl, file)
+	if m is None:
+		print("Error: invalid name of file %s" % file, file=sys.stderr)
+		return 1
+
+	fields = [
+			(cfg.number, TRCK, m.group(1)),
+			(cfg.artist, TPE1, m.group(2)),
+			(cfg.name,   TIT2, m.group(3)),
+			*(cfg.sameartist and [ cfg.albumartist, TPE2, m.group(2) ] or [])
+	]
+
+	ret = 0
+	for check, frame, value in fields:
+		if check is not None:
+			continue
+		ret |= set_field(cfg, frame, value, file)
+
+	return ret
+
+
+
+def set_fields(cfg, file):
+	ret = 0
+	for frame, value in cfg.field:
+		ret |= set_field(cfg, frame, value, file)
+	return ret
+
+
+
+def add_genres(cfg, file):
+	genres  = subprocess.check_output(cfg.getcom % (file, TCON))
+	genres  = list(map(lambda x: x.strip(), re.split(r'[^\w\s]', genres)))
+	genres += list(map(lambda x: x.strip(), re.split(r'[^\w\s]', cfg.addgenres)))
+	cfg.sortgenres and genres.sort()
+	genres  = cfg.genredelimeter.join(genres)
+	return set_field(cfg, TCON, genres, file)
+
+
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# ~~~~~                              MAIN                              ~~~~~ #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
 def main():
